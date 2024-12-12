491dc7d3cbc268fe5c2dfde3240edae20d755b21
magneum-II <yt-dlx@proton.me>
2024-12-12 08:46:42 +0530

    picbook

---
+++ backup/tailwind.config.ts
@@ -0,0 +1,10 @@
+import daisyui from "daisyui";
+import type { Config } from "tailwindcss";
+import tailwindScrollbar from "tailwind-scrollbar";
+import aspectRatio from "@tailwindcss/aspect-ratio";
+import tailwindScrollbarHide from "tailwind-scrollbar-hide";
+export default {
+  plugins: [daisyui, tailwindScrollbarHide, tailwindScrollbar({ nocompatible: true }), aspectRatio],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: { extend: { aspectRatio: { "h-9": "9", "w-16": "16" }, fontFamily: { Kurale: ["var(--font-Kurale)"], Brittany: ["var(--font-Brittany)"] } } }
+} satisfies Config;
