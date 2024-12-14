038bc925f05409a497537384dd94465fd8b7e777
magneum-II <yt-dlx@proton.me>
2024-12-14 22:27:35 +0530

    picbook

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,9 @@
 import daisyui from "daisyui";
 import type { Config } from "tailwindcss";
-import tailwindScrollbar from "tailwind-scrollbar";
-import aspectRatio from "@tailwindcss/aspect-ratio";
-import tailwindScrollbarHide from "tailwind-scrollbar-hide";
 
 export default {
   darkMode: "class",
-  plugins: [daisyui, tailwindScrollbarHide, tailwindScrollbar({ nocompatible: true }), aspectRatio],
-  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: { extend: { aspectRatio: { "h-9": "9", "w-16": "16" }, fontFamily: { Kurale: ["var(--font-Kurale)"], Brittany: ["var(--font-Brittany)"] } } }
+  plugins: [daisyui],
+  theme: { extend: { ontFamily: { Kurale: ["var(--font-Kurale)"], Brittany: ["var(--font-Brittany)"] } } },
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"]
 } satisfies Config;
