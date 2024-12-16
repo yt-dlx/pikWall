8e639b70732459783dc481c65476fc5962ba1b82
magneum-II <yt-dlx@proton.me>
2024-12-16 18:12:10 +0530

    picbook

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import withPWAInit from "@ducanh2912/next-pwa";
-const withPWA = withPWAInit({
-  dest: "public"
-});
+const withPWA = withPWAInit({ dest: "public" });
 export default withPWA({
   reactStrictMode: true,
   images: { remotePatterns: [{ protocol: "https", hostname: "**" }] }
