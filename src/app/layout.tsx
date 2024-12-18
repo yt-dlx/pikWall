4baf28bc3c7866d19f9280234d79247a50a1aadc
magneum-II <yt-dlx@proton.me>
2024-12-18 12:09:00 +0530

    picbook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 // src/app/layout.tsx
 import "./globals.css";
-import Script from "next/script";
 import { Providers } from "./providers";
 import LocalFontLoader from "next/font/local";
 import type { Metadata, Viewport } from "next";
+import AdSense from "@/components/AdSense";
 
 const Kurale = LocalFontLoader({ variable: "--font-Kurale", src: "../fonts/Kurale.ttf" });
 const Brittany = LocalFontLoader({ variable: "--font-Brittany", src: "../fonts/Brittany.otf" });
@@ -117,11 +117,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <AdSense />
+      </head>
       <body className={`${Kurale.variable} ${Brittany.variable} antialiased font-Kurale capitalize`}>
-        <Providers>
-          <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9464475307933754" crossOrigin="anonymous" />
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
---
+++ src/components/AdSense.tsx
@@ -0,0 +1,6 @@
+import Script from "next/script";
+import React from "react";
+const AdSense = () => {
+  return <Script async crossOrigin="anonymous" strategy="afterInteractive" src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-${process.env.pub_id}`} />;
+};
+export default AdSense;
