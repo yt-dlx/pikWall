2b89c23bac734949d7347d34f13bb22be702b16a
magneum-II <yt-dlx@proton.me>
2024-12-19 14:17:20 +0530

    picbook

--- src/components/AdSense.tsx
+++ src/components/AdSense.tsx
@@ -2,6 +2,6 @@
 import Script from "next/script";
 import React from "react";
 const AdSense = () => {
-  return <Script async crossOrigin="anonymous" strategy="afterInteractive" src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-${process.env.pub_id}`} />;
+  return <Script async crossOrigin="anonymous" strategy="afterInteractive" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9464475307933754" />;
 };
 export default AdSense;
