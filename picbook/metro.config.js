c0aed21321e961bbdc2b519a036bbf91645580b5
magneum-II <yt-dlx@proton.me>
2024-12-19 09:34:30 +0530

    picbook

---
+++ picbook/metro.config.js
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
+const config = getDefaultConfig(__dirname);
+module.exports = withNativeWind(config, { input: "./global.css" });
