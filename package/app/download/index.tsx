ab194d3d4f039573b3b72cf631c1023cb480bceb
magneum-II <yt-dlx@proton.me>
2024-12-21 19:11:45 +0530

    ❣️picbook by shovit

--- package/app/download/index.tsx
+++ package/app/download/index.tsx
@@ -3,7 +3,7 @@ import { ImageMetadata } from "../../types/types";
 import { MaterialIcons } from "@expo/vector-icons";
 import { useLocalSearchParams } from "expo-router";
 import React, { useState, useRef, useEffect } from "react";
-import { View, Image, Text, TouchableOpacity, ScrollView, Alert, ActivityIndicator, Dimensions, Animated } from "react-native";
+import { View, Image, Text, TouchableOpacity, ScrollView, Alert, ActivityIndicator, Dimensions, Animated, Easing } from "react-native";
 //  =======================================================================================
 //  =======================================================================================
 interface ImageProps {
@@ -22,11 +22,36 @@ const PreviewImage: React.FC<ImageProps> = ({ ImageData, screenWidth }) => {
   const scaleValue = useRef(new Animated.Value(1)).current;
   const rotateValue = useRef(new Animated.Value(0)).current;
   useEffect(() => {
+    // Scaling animation
     Animated.loop(
-      Animated.sequence([Animated.timing(scaleValue, { toValue: 1.2, duration: 1000, useNativeDriver: true }), Animated.timing(scaleValue, { toValue: 1, duration: 2000, useNativeDriver: true })])
+      Animated.sequence([
+        Animated.timing(scaleValue, {
+          toValue: 1.4,
+          duration: 4000,
+          useNativeDriver: true
+        }),
+        Animated.timing(scaleValue, {
+          toValue: 1.1,
+          duration: 2000,
+          useNativeDriver: true
+        })
+      ]),
+      { resetBeforeIteration: false } // Prevent resetting values before next loop
     ).start();
-    Animated.loop(Animated.timing(rotateValue, { toValue: 1, duration: 6000, useNativeDriver: true })).start();
+
+    // Rotating animation
+    rotateValue.setValue(0); // Reset rotation explicitly to ensure smooth loop
+    Animated.loop(
+      Animated.timing(rotateValue, {
+        toValue: 1,
+        duration: 4000,
+        useNativeDriver: true,
+        easing: Easing.linear // Ensure continuous rotation
+      }),
+      { resetBeforeIteration: false }
+    ).start();
   }, [scaleValue, rotateValue]);
+
   const rotateInterpolate = rotateValue.interpolate({
     inputRange: [0, 1],
     outputRange: ["0deg", "360deg"]
@@ -34,14 +59,24 @@ const PreviewImage: React.FC<ImageProps> = ({ ImageData, screenWidth }) => {
   return (
     <View className="relative">
       <View className="absolute inset-0 z-50 flex justify-center items-center">
-        <Animated.View style={{ transform: [{ scale: scaleValue }, { rotate: rotateInterpolate }] }} className="rounded-full flex items-center">
-          <Image style={{ backgroundColor: ImageData.primary }} alt="logo" source={require("../../assets/picbook/picbook_white_nobg.png")} className="w-10 h-10 rounded-full" resizeMode="contain" />
-        </Animated.View>
-        <Text style={{ fontFamily: "Kurale" }} className="text-xl font-black text-white tracking-tight">
-          picBook™
-        </Text>
+        {!imageLoading && (
+          <>
+            <Animated.View style={{ transform: [{ scale: scaleValue }, { rotate: rotateInterpolate }] }} className="rounded-full flex items-center">
+              <Image
+                source={require("../../assets/picbook/picbook_white_nobg.png")}
+                style={{ backgroundColor: ImageData.primary }}
+                className="w-10 h-10 rounded-full"
+                resizeMode="contain"
+                alt="logo"
+              />
+            </Animated.View>
+            <Text style={{ fontFamily: "Kurale" }} className="text-xl font-black text-white tracking-tight">
+              picBook™
+            </Text>
+          </>
+        )}
       </View>
-      <View style={{ borderColor: ImageData.primary, borderWidth: 1, borderRadius: 15, overflow: "hidden" }} className="shadow-lg">
+      <View style={{ borderColor: ImageData.primary, borderWidth: 1, borderTopLeftRadius: 20, borderTopRightRadius: 20, overflow: "hidden" }} className="shadow-lg">
         {imageLoading && (
           <View style={{ height: imageHeight, justifyContent: "center", alignItems: "center", backgroundColor: "#0A0A0A" }}>
             <ActivityIndicator size="large" color={ImageData.primary} />
@@ -49,8 +84,8 @@ const PreviewImage: React.FC<ImageProps> = ({ ImageData, screenWidth }) => {
           </View>
         )}
         <View className="relative">
-          <Image
-            style={!imageLoading ? { width: imageWidth, height: imageHeight, borderRadius: 15 } : { width: 0, height: 0 }}
+          <Animated.Image
+            style={!imageLoading ? { width: imageWidth, height: imageHeight, borderTopLeftRadius: 20, borderTopRightRadius: 20, transform: [{ scale: scaleValue }] } : { width: 0, height: 0 }}
             source={{ uri: ImageData.previewLink.replace("lowRes", "highRes") }}
             onLoadStart={() => setImageLoading(true)}
             onLoadEnd={() => setImageLoading(false)}
@@ -63,7 +98,7 @@ const PreviewImage: React.FC<ImageProps> = ({ ImageData, screenWidth }) => {
             }}
           />
         </View>
-        <View style={{ backgroundColor: ImageData.primary + "80" }} className="absolute bottom-0 left-0 right-0 backdrop-blur-sm p-0.5 rounded-b-2xl">
+        <View style={{ backgroundColor: ImageData.primary + "80" }} className="absolute bottom-0 left-0 right-0 backdrop-blur-sm p-1">
           <Text style={{ fontFamily: "Kurale" }} className="text-white font-semibold text-lg text-center">
             {ImageData.original_file_name.replace(".jpg", "")}
           </Text>
@@ -87,21 +122,22 @@ export default function DownloadScreen() {
   };
   return (
     <ScrollView style={{ backgroundColor: "#0A0A0A", flex: 1 }}>
-      <View style={{ padding: 8 }}>
+      <View style={{ padding: 10 }}>
         <PreviewImage ImageData={ImageData} isHorizontal={true} screenWidth={screenWidth} screenHeight={screenHeight} />
         <TouchableOpacity
           disabled={isDownloading}
           onPress={handleDownload}
           style={{
-            backgroundColor: ImageData.primary + "90",
+            marginTop: 2,
             paddingVertical: 8,
-            paddingHorizontal: 8,
-            flexDirection: "row",
-            justifyContent: "center",
             alignItems: "center",
-            marginTop: 4,
+            flexDirection: "row",
+            paddingHorizontal: 8,
+            justifyContent: "center",
+            borderBottomLeftRadius: 20,
+            borderBottomRightRadius: 20,
             opacity: isDownloading ? 0.5 : 1,
-            borderRadius: 10
+            backgroundColor: ImageData.primary
           }}
         >
           <MaterialIcons name="file-download" size={15} color="#ffffff" />
