96f09da509e996cf9c82e3435a3e8a5a4a912ac7
magneum-II <yt-dlx@proton.me>
2024-12-22 20:59:24 +0530

    ❣️picbook by shovit

--- package/components/HeaderAnimate.tsx
+++ package/components/HeaderAnimate.tsx
@@ -1,8 +1,48 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
-"../global.css";
+const imageSets: string[][] = [
+  [
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Soft Glow Of Pastel Skies (1).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Soft Glow Of Pastel Skies (2).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Tranquility Under Emerald Canopy (1).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Tranquility Under Emerald Canopy (2).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Dreamscape Of Blush Meadows (1).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Dreamscape Of Blush Meadows (2).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Crimson Glow Over Silent Steppe (1).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Crimson Glow Over Silent Steppe (2).jpg"
+  ],
+  [
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Tranquility Under Emerald Canopy (3).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Tranquility Under Emerald Canopy (4).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Dreamscape Of Blush Meadows (3).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Dreamscape Of Blush Meadows (4).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Crimson Glow Over Silent Steppe (3).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Crimson Glow Over Silent Steppe (4).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Soft Glow Of Pastel Skies (3).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Soft Glow Of Pastel Skies (4).jpg"
+  ],
+  [
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Dreamscape Of Blush Meadows (1).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Dreamscape Of Blush Meadows (4).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Crimson Glow Over Silent Steppe (1).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Crimson Glow Over Silent Steppe (4).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Soft Glow Of Pastel Skies (1).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Soft Glow Of Pastel Skies (4).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Tranquility Under Emerald Canopy (1).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Tranquility Under Emerald Canopy (4).jpg"
+  ],
+  [
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Crimson Glow Over Silent Steppe (2).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Crimson Glow Over Silent Steppe (3).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Soft Glow Of Pastel Skies (2).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Soft Glow Of Pastel Skies (3).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Tranquility Under Emerald Canopy (2).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Tranquility Under Emerald Canopy (3).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Dreamscape Of Blush Meadows (2).jpg",
+    "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Dreamscape Of Blush Meadows (3).jpg"
+  ]
+];
 import React, { useEffect } from "react";
-import { Text, View } from "react-native";
-import { Image, StyleSheet } from "react-native";
+import { Text, View, Image } from "react-native";
 import Animated, { useSharedValue, useAnimatedStyle, withTiming, withRepeat, withSequence, withDelay } from "react-native-reanimated";
 
 interface ScrollingSlotProps {
@@ -12,7 +52,7 @@ interface ScrollingSlotProps {
 }
 
 const ScrollingSlot: React.FC<ScrollingSlotProps> = ({ images, reverse, delay }) => {
-  const imageHeight = 144;
+  const imageHeight = 96;
   const totalHeight = images.length * imageHeight;
   const scrollValue = useSharedValue(0);
   const opacity = useSharedValue(0);
@@ -20,15 +60,12 @@ const ScrollingSlot: React.FC<ScrollingSlotProps> = ({ images, reverse, delay })
     opacity.value = withDelay(delay, withTiming(1, { duration: 1000 }));
     scrollValue.value = withDelay(delay, withRepeat(withTiming(totalHeight, { duration: 15000 }), -1, reverse));
   }, [scrollValue, totalHeight, reverse, delay, opacity]);
-  const animatedStyle = useAnimatedStyle(() => ({
-    transform: [{ translateY: -scrollValue.value % totalHeight }],
-    opacity: opacity.value
-  }));
+  const animatedStyle = useAnimatedStyle(() => ({ transform: [{ translateY: -scrollValue.value % totalHeight }], opacity: opacity.value }));
   return (
     <View className="flex-1 overflow-hidden px-1">
       <Animated.View style={animatedStyle} className="flex-col">
         {images.concat(images).map((uri: string, idx: number) => (
-          <Image key={idx} alt="image" source={{ uri }} className="w-full h-36 rounded-lg mb-2" resizeMode="cover" blurRadius={1.2} />
+          <Image key={idx} source={{ uri }} alt="image" className="w-full h-44 rounded-lg mb-1" resizeMode="cover" blurRadius={1.0} />
         ))}
       </Animated.View>
     </View>
@@ -36,86 +73,42 @@ const ScrollingSlot: React.FC<ScrollingSlotProps> = ({ images, reverse, delay })
 };
 
 const HeaderAnimate: React.FC = () => {
-  const imageSets: string[][] = [
-    [
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Soft Glow Of Pastel Skies (1).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Soft Glow Of Pastel Skies (2).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Tranquility Under Emerald Canopy (1).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Tranquility Under Emerald Canopy (2).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Dreamscape Of Blush Meadows (1).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Dreamscape Of Blush Meadows (2).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Crimson Glow Over Silent Steppe (1).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Crimson Glow Over Silent Steppe (2).jpg"
-    ],
-    [
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Tranquility Under Emerald Canopy (3).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Tranquility Under Emerald Canopy (4).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Dreamscape Of Blush Meadows (3).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Dreamscape Of Blush Meadows (4).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Crimson Glow Over Silent Steppe (3).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Crimson Glow Over Silent Steppe (4).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Soft Glow Of Pastel Skies (3).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Soft Glow Of Pastel Skies (4).jpg"
-    ],
-    [
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Dreamscape Of Blush Meadows (1).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Dreamscape Of Blush Meadows (4).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Crimson Glow Over Silent Steppe (1).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Crimson Glow Over Silent Steppe (4).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Soft Glow Of Pastel Skies (1).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Soft Glow Of Pastel Skies (4).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Tranquility Under Emerald Canopy (1).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Tranquility Under Emerald Canopy (4).jpg"
-    ],
-    [
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Crimson Glow Over Silent Steppe (2).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Crimson Glow Over Silent Steppe (3).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Soft Glow Of Pastel Skies (2).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Soft Glow Of Pastel Skies (3).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Tranquility Under Emerald Canopy (2).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Tranquility Under Emerald Canopy (3).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Dreamscape Of Blush Meadows (2).jpg",
-      "https://raw.githubusercontent.com/yt-dlx/picbook/lowRes/Dreamscape Of Blush Meadows (3).jpg"
-    ]
-  ];
   const AnimatedTitle: React.FC = () => {
     const scale = useSharedValue(0.95);
     useEffect(() => {
       scale.value = withRepeat(withSequence(withTiming(1.05, { duration: 2000 }), withTiming(0.95, { duration: 2000 })), -1, true);
     }, [scale]);
-    const animatedStyle = useAnimatedStyle(() => ({
-      transform: [{ scale: scale.value }]
-    }));
+    const animatedStyle = useAnimatedStyle(() => ({ transform: [{ scale: scale.value }] }));
     return (
       <Animated.View style={animatedStyle} className="items-center">
-        <View className=" bg-black/60 rounded-full">
-          <Image source={require("@/assets/picbook/white_nobg_1024.png")} alt="logo" className="w-16 h-16 border-white border-2 rounded-full" resizeMode="contain" />
+        <View className="bg-black/60 rounded-full">
+          <Image source={require("@/assets/picbook/white_nobg_1024.png")} alt="logo" className="w-12 h-12 border-2 border-white rounded-full" resizeMode="contain" />
         </View>
-        <Text style={{ fontFamily: "Kurale" }} className="text-7xl font-black text-white tracking-tight">
+        <Text style={{ fontFamily: "Kurale" }} className="text-4xl font-black text-white tracking-tight">
           picBook™
         </Text>
       </Animated.View>
     );
   };
   return (
-    <View className="flex-1 items-center justify-center m-4">
-      <View style={{ height: 300 }} className="flex-row overflow-hidden rounded-xl relative">
-        {imageSets.map((images, slotIndex) => {
-          return <ScrollingSlot key={slotIndex} images={images} reverse={slotIndex % 2 === 0} delay={slotIndex * 200} />;
-        })}
-        <View style={styles.overlay} className="rounded-2xl text-center">
-          <View style={styles.blurredBackground} />
-          <View className="absolute justify-center items-center m-4 p-2">
-            <View className="flex-row mb-2">
+    <View className="flex-1 items-center justify-center m-2">
+      <View style={{ height: 220 }} className="flex-row overflow-hidden rounded-xl relative h-[200px]">
+        {imageSets.map((images, slotIndex) => (
+          <ScrollingSlot key={slotIndex} images={images} reverse={slotIndex % 2 === 0} delay={slotIndex * 200} />
+        ))}
+        <View className="absolute inset-0 items-center justify-center rounded-[8px] overflow-hidden">
+          <View className="absolute inset-0 bg-[#0A0A0A] opacity-50 rounded-[8px]" />
+          <View className="absolute justify-center items-center m-2 p-1">
+            <View className="flex-row mb-1">
               <AnimatedTitle />
             </View>
-            <View className="flex-row items-center mt-4 bg-black/30 px-4 py-2 rounded-full">
-              <View className="w-2 h-2 rounded-full bg-white mr-2 animate-pulse" />
-              <Text style={{ fontFamily: "Kurale" }} className="text-sm text-white font-semibold">
+            <View className="flex-row items-center mt-2 bg-black/30 px-2 py-1 rounded-full">
+              <View className="w-1.5 h-1.5 rounded-full bg-white mr-1 animate-pulse" />
+              <Text style={{ fontFamily: "Kurale" }} className="text-xs text-white font-semibold">
                 Crafted with imagination and stories
               </Text>
             </View>
-            <Text style={{ fontFamily: "Kurale" }} className="text-xl text-gray-300 mt-4 leading-7 font-medium text-center">
+            <Text style={{ fontFamily: "Kurale" }} className="text-base text-gray-300 mt-2 leading-6 font-medium text-center">
               Dive into tales inspired by unique images and discover the art of visual environment telling.
             </Text>
           </View>
@@ -124,10 +117,5 @@ const HeaderAnimate: React.FC = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  blurredBackground: { position: "absolute", top: 0, left: 0, right: 0, bottom: 0, backgroundColor: "#0A0A0A", opacity: 0.5, borderRadius: 15 },
-  overlay: { position: "absolute", top: 0, left: 0, right: 0, bottom: 0, alignItems: "center", justifyContent: "center", borderRadius: 10, overflow: "hidden" }
-});
 
 export default HeaderAnimate;
