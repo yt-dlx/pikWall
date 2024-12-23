dedf58dd6d43fe9a93dea62a2a1fe41e24a4e92a
magneum-II <yt-dlx@proton.me>
2024-12-23 11:34:03 +0530

    ❣️picbook by shovit

--- package/app/index.tsx
+++ package/app/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { View } from "react-native";
-import DownloadAndSaveImage from "@/components/Downloader";
+import DownloadAndSaveImage from "@/components/DownloadAndSaveImage";
 
 const App = () => {
   return (
-    <View className="flex-1">
+    <View style={{ flex: 1, backgroundColor: "#0A0A0A" }}>
       <DownloadAndSaveImage url="https://raw.githubusercontent.com/yt-dlx/picbook/highRes/Abstract%20Symphony%20In%20Painted%20Passage%20(2).jpg" />
     </View>
   );
