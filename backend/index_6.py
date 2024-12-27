2992893685aaa7e4783364e566efcaf229a26201
magneum-II <yt-dlx@proton.me>
2024-12-27 10:01:57 +0530

    ❣️picbook by shovit

--- backend/index_6.py
+++ backend/index_6.py
@@ -18,8 +18,31 @@ def rescale_images(source_dir, target_dir, width, height):
                 print(f"Rescaled and saved: {target_path}")
         except Exception as e:
             print(f"Failed to process {file_path}: {e}")
+
 if __name__ == "__main__":
-    source_dir=os.path.join("sources", "highRes")
-    target_dir=os.path.join("sources", "lowRes")
-    rescale_images(source_dir, target_dir, 640 , 360)
+    print("Choose the image format:")
+    print("1: Landscape (640 x 360)")
+    print("2: Portrait (360 x 640)")
+    choice = input("Enter 1 for Landscape or 2 for Portrait: ")
+    if choice == "1":
+        width, height = 640, 360
+    elif choice == "2":
+        width, height = 360, 640
+    else:
+        print("Invalid choice. Exiting the program.")
+        exit()
+# ==================================================XXX==================================================
+    source_dir = os.path.join("sources", "output", "Anime", "highRes")
+    # source_dir = os.path.join("sources", "output", "Portrait", "highRes")
+    # source_dir = os.path.join("sources", "output", "Lightning", "highRes")
+    # source_dir = os.path.join("sources", "output", "Cinematic", "highRes")
+    # source_dir = os.path.join("sources", "output", "Photography", "highRes")
+# ==================================================XXX==================================================
+    # target_dir = os.path.join("sources", "output", "Photography", "lowRes")
+    # target_dir = os.path.join("sources", "output", "Cinematic", "lowRes")
+    # target_dir = os.path.join("sources", "output", "Lightning", "lowRes")
+    # target_dir = os.path.join("sources", "output", "Portrait", "lowRes")
+    target_dir = os.path.join("sources", "output", "Anime", "lowRes")
+# ==================================================XXX==================================================
+    rescale_images(source_dir, target_dir, width, height)
 # ==================================================XXX==================================================
