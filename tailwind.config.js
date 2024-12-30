b357a11b014a5e309853c9f9400f296b50fd5123
magneum-II <yt-dlx@proton.me>
2024-12-30 21:03:46 +0530

    rm

--- tailwind.config.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-/** @type {import('tailwindcss').Config} */
-module.exports = { content: ["./src/**/*.{js,jsx,ts,tsx}"], presets: [require("nativewind/preset")], theme: { extend: {} }, plugins: [] };
