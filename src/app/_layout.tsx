29ce0d799264a0aa49787c56b489ef19348e9a0b
magneum-II <yt-dlx@proton.me>
2025-01-01 01:54:58 +0530

    picWall AI

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -9,9 +9,9 @@ import { LinearGradient } from "expo-linear-gradient";
 import { SafeAreaView, StatusBar, View } from "react-native";
 // ============================================================================================
 // ============================================================================================
+SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
-  SplashScreen.preventAutoHideAsync();
-  const [fontsLoaded, fontsError] = useFonts({
+  const [loaded, error] = useFonts({
     Caveat_Bold: require("@/assets/fonts/Caveat_Bold.ttf"),
     Kurale_Regular: require("@/assets/fonts/Kurale_Regular.ttf"),
     Caveat_Regular: require("@/assets/fonts/Caveat_Regular.ttf"),
@@ -27,9 +27,9 @@ export default function RootLayout() {
     Linotte_Semi_Bold: require("@/assets/fonts/Linotte_Semi_Bold.otf")
   });
   React.useEffect(() => {
-    if (fontsLoaded || fontsError) SplashScreen.hideAsync();
-  }, [fontsLoaded, fontsError]);
-  if (!fontsLoaded && !fontsError) return null;
+    if (loaded || error) SplashScreen.hideAsync();
+  }, [loaded, error]);
+  if (!loaded && !error) return null;
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: Colorizer("#000000", 1.0) }}>
       <StatusBar backgroundColor="#000000" barStyle="light-content" />
@@ -37,12 +37,8 @@ export default function RootLayout() {
         colors={[Colorizer("#000000", 1.0), Colorizer("#000000", 0.8), Colorizer("#000000", 0.6), Colorizer("#000000", 0.4), Colorizer("#000000", 0.2), "transparent"]}
         style={{ position: "absolute", top: 0, left: 0, right: 0, height: 100, zIndex: 50 }}
       />
-      <View style={{ flex: 1, backgroundColor: Colorizer("#000000", 1.0) }}>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="index" options={{ title: "Home" }} />
-          <Stack.Screen name="Home" options={{ title: "Home Screen" }} />
-          <Stack.Screen name="Image" options={{ title: "Image Screen" }} />
-        </Stack>
+      <View style={{ flex: 1, backgroundColor: Colorizer("#000000", 1.0) }} className="capitalize">
+        <Stack screenOptions={{ headerShown: false }} />
       </View>
     </SafeAreaView>
   );
