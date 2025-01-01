e9213ce13e510fbfcfbdf3abb4442a8aa2b95d96
magneum-II <yt-dlx@proton.me>
2025-01-01 16:16:02 +0530

    picWall

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -13,7 +13,6 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const router = useRouter();
-  const { lastState, clearState } = useAppState();
   const [loaded, error] = useFonts({
     Caveat_Bold: require("@/assets/fonts/Caveat_Bold.ttf"),
     Kurale_Regular: require("@/assets/fonts/Kurale_Regular.ttf"),
@@ -29,22 +28,23 @@ export default function RootLayout() {
     Dm_Serif_Display_Italic: require("@/assets/fonts/Dm_Serif_Display_Italic.ttf"),
     Dm_Serif_Display_Regular: require("@/assets/fonts/Dm_Serif_Display_Regular.ttf")
   });
-
   React.useEffect(() => {
     const initializeApp = async () => {
-      if (loaded || error) {
+      if (loaded && !error) {
         await SplashScreen.hideAsync();
-        if (lastState) {
-          router.replace({ pathname: "/Image", params: { data: JSON.stringify({ data: lastState.data, selectedIndex: lastState.selectedIndex, environment_title: lastState.environment_title }) } });
-          clearState();
-        }
+        const { lastState, hasRedirected, setRedirected, clearState } = useAppState.getState();
+        if (lastState && !hasRedirected) {
+          router.push({
+            pathname: "/Image",
+            params: { data: JSON.stringify({ data: lastState.data, selectedIndex: lastState.selectedIndex, environment_title: lastState.environment_title || "Default Title" }) }
+          });
+          setRedirected(true);
+        } else if (hasRedirected) clearState();
       }
     };
     initializeApp();
-  }, [loaded, error, lastState, clearState]);
-
+  }, [loaded, error]);
   if (!loaded && !error) return null;
-
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: Colorizer("#000000", 1.0) }}>
       <StatusBar backgroundColor="#000000" barStyle="light-content" />
